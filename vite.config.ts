e03d3831eca95c09c17a1285bab8045585d1e57f
zhangpeng <1121027880@qq.com>
2024-01-05 15:21:57 +0800

    feat: 别名设置

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,23 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
 
+import * as path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		//设置别名
+		alias: {
+			"@": path.resolve(__dirname, "src"),
+		},
+	},
 	plugins: [
 		vue(),
 		eslintPlugin({
 			include: ["src/**/*.ts", "src/**/*.vue", "src/*.ts", "src/*.vue"],
 		}),
 	],
+	server: {
+		host: true,
+	},
 });
